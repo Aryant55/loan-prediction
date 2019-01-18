044826804a37223a91677f586a2bc22b28e10cb7
aryant shukla <aeyantshukla@gmail.com>
2019-01-18 13:09:17 +0530

    Update zxc.py

--- zxc.py
+++ zxc.py
@@ -6,8 +6,134 @@ import pandas as pd
 
 train = pd.read_csv('train.csv')
 train_original=train.copy()
-
+test = pd.read_csv('test.csv')
+l = pd.read_csv('logistic.csv')
+test_original = pd.read_csv('test.csv')
 train['Loan_Status'].value_counts().plot.bar()
+
+
+#imputer
+train.isnull().sum()
+train['Gender'].fillna(train['Gender'].mode()[0], inplace=True)
+train['Married'].fillna(train['Married'].mode()[0], inplace=True)
+train['Dependents'].fillna(train['Dependents'].mode()[0], inplace=True)
+train['Self_Employed'].fillna(train['Self_Employed'].mode()[0], inplace=True)
+train['Credit_History'].fillna(train['Credit_History'].mode()[0], inplace=True)
+
+train['Loan_Amount_Term'].fillna(train['Loan_Amount_Term'].mode()[0],inplace=True)
+
+train['LoanAmount'].fillna(train['LoanAmount'].median(),inplace=True)
+
+test.isnull().sum()
+test['Gender'].fillna(train['Gender'].mode()[0], inplace=True)
+test['Dependents'].fillna(train['Dependents'].mode()[0], inplace=True)
+test['Self_Employed'].fillna(train['Self_Employed'].mode()[0], inplace=True)
+test['Credit_History'].fillna(train['Credit_History'].mode()[0], inplace=True)
+test['Loan_Amount_Term'].fillna(train['Loan_Amount_Term'].mode()[0], inplace=True)
+test['LoanAmount'].fillna(train['LoanAmount'].median(), inplace=True)
+
+
+#drop axis=1
+train=train.drop('Loan_ID',axis=1)
+test=test.drop('Loan_ID',axis=1)
+
+X = train.drop('Loan_Status',1)
+y = train.Loan_Status
+
+
+X=pd.get_dummies(X)
+train=pd.get_dummies(train)
+test=pd.get_dummies(test)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+test = sc_X.transform(test)
+
+
+from sklearn.model_selection import train_test_split
+x_train, x_cv, y_train, y_cv = train_test_split(X,y, test_size =0.3)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+model = LogisticRegression()
+model.fit(x_train, y_train)
+pred_cv = model.predict(x_cv)
+accuracy_score(y_cv,pred_cv)
+
+
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+model.fit(x_train, y_train)
+pred_cv = model.predict(x_cv)
+accuracy_score(y_cv,pred_cv)
+
+
+from sklearn.svm import SVC
+model = SVC(kernel = 'rbf', random_state = 0)
+model.fit(x_train, y_train)
+pred_cv = model.predict(x_cv)
+accuracy_score(y_cv,pred_cv)
+
+pred_test = model.predict(test)
+
+
+
+train['Total_Income']=train['ApplicantIncome']+train['CoapplicantIncome']
+test['Total_Income']=test['ApplicantIncome']+test['CoapplicantIncome']
+sns.distplot(train['Total_Income']);
+train['Total_Income_log'] = np.log(train['Total_Income'])
+sns.distplot(train['Total_Income_log']);
+test['Total_Income_log'] = np.log(test['Total_Income'])
+train['EMI']=train['LoanAmount']/train['Loan_Amount_Term']
+test['EMI']=test['LoanAmount']/test['Loan_Amount_Term']
+train['Balance Income']=train['Total_Income']-(train['EMI']*1000)  
+test['Balance Income']=test['Total_Income']-(test['EMI']*1000)
+sns.distplot(train['Balance Income']);
+train=train.drop(['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term'], axis=1)
+test=test.drop(['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term'], axis=1)
+
+
+from sklearn.model_selection import StratifiedKFold
+i=1
+kf = StratifiedKFold(n_splits=5,random_state=1,shuffle=True)
+for train_index,test_index in kf.split(X,y):
+     print('\n{} of kfold {}'.format(i,kf.n_splits))
+     xtr,xvl = X.loc[train_index],X.loc[test_index]
+     ytr,yvl = y[train_index],y[test_index]
+     model = LogisticRegression(random_state=1)
+     model.fit(xtr, ytr)
+     pred_test = model.predict(xvl)
+     score = accuracy_score(yvl,pred_test)
+     print('accuracy_score',score)
+     i+=1
+pred_test = model.predict(test)
+pred=model.predict_proba(xvl)[:,1]
+
+i=1
+kf = StratifiedKFold(n_splits=5,random_state=1,shuffle=True)
+for train_index,test_index in kf.split(X,y):
+     print('\n{} of kfold {}'.format(i,kf.n_splits))
+     xtr,xvl = X.loc[train_index],X.loc[test_index]
+     ytr,yvl = y[train_index],y[test_index]
+    
+     model = LogisticRegression(random_state=1)
+     model.fit(xtr, ytr)
+     pred_test = model.predict(xvl)
+     score = accuracy_score(yvl,pred_test)
+     print('accuracy_score',score)
+     i+=1
+pred_test = model.predict(test)
+pred=model.predict_proba(xvl)[:,1]
+
+submission=pd.read_csv("Sample_Submission.csv")
+submission['Loan_Status']=pred_test
+submission['Loan_ID']=test_original['Loan_ID']
+
+submission['Loan_Status'].replace(0, 'N',inplace=True)
+submission['Loan_Status'].replace(1, 'Y',inplace=True)
+
+pd.DataFrame(submission, columns=['Loan_ID','Loan_Status']).to_csv('logistic.csv')
 
 #independent variable categorical
 plt.figure(1)
@@ -48,6 +174,11 @@ plt.subplot(122)
 train['ApplicantIncome'].plot.box(figsize=(16,5))
 
 plt.show()
+
+
+
+
+
 
 '''
 X = dataset.iloc[:, [2, 3]].values
